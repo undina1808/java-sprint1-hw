3e52b5b8bea0808a57e6591bf692f6fb53c05bdb
evorotilova <undina1808@yandex.ru>
2022-10-20 23:56:10 +0300

    Внесены промежуточные правки по ревью2 тз1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         StepTracker steptracker = new StepTracker();
 
         System.out.println("Вы находитесь в главном меню программы <Счётчик калорий>");
@@ -12,50 +11,11 @@ public class Main {
 
         while (userInput != 0) {
             if (userInput == 1) {
-                System.out.println("Укажите месяц (считаем, что январь - 0-ой месяц)");
-                int month = scanner.nextInt();
-                while (month < 0 || month > 11) {
-                    System.out.println("Значение месяца должно быть неотрицательным и не превышать 11");
-                    System.out.println("Укажите месяц (считаем, что январь - 0-ой месяц)");
-                    month = scanner.nextInt();
-                }
-
-                System.out.println("Укажите день");
-                int day = scanner.nextInt();
-                while (day < 1 || day > 30) {
-                    System.out.println("Значение дня должно быть положительным и не превышать 30");
-                    System.out.println("Укажите день");
-                    day = scanner.nextInt();
-                }
-
-                System.out.println("Укажите количество шагов");
-                int steps = scanner.nextInt();
-                while (steps < 0) {
-                    System.out.println("Количество шагов должно быть неотрицательным");
-                    System.out.println("Укажите количество шагов");
-                    steps = scanner.nextInt();
-                }
-                steptracker.addSteps(month, day, steps);
-
+                inputSteps(scanner, steptracker);
             } else if (userInput == 2) {
-                System.out.println("Укажите месяц, за который вывести статистику");
-                int month = scanner.nextInt();
-                while (month < 0 || month > 11) {
-                    System.out.println("Значение месяца должно быть неотрицательным и не превышать 11");
-                    System.out.println("Укажите месяц");
-                    month = scanner.nextInt();
-                }
-                steptracker.printStatistics(month);
-
+                outputStatistics(scanner, steptracker);
             } else if (userInput == 3) {
-                System.out.println("Задайте дневную цель по шагам");
-                int stepsGoal = scanner.nextInt();
-                while (stepsGoal <= 0) {
-                    System.out.println("Дневная цель должна быть больше или равна 0");
-                    System.out.println("Задайте дневную цель по шагам");
-                    stepsGoal = scanner.nextInt();
-                }
-                steptracker.modifyGoal(stepsGoal);
+                setGoal(scanner, steptracker);
             } else {
                 System.out.println("Извините, такой команды нет. Возврат в главное меню");
             }
@@ -73,7 +33,52 @@ public class Main {
         System.out.println("0 - Выйти из программы");
     }
 
-    private static boolean checkValues () {
+    private static void inputSteps(Scanner scanner, StepTracker steptracker) {
+        System.out.println("Укажите месяц (считаем, что январь - 0-ой месяц)");
+        int month = scanner.nextInt();
+        while (month < 0 || month > 11) {
+            System.out.println("Значение месяца должно быть неотрицательным и не превышать 11");
+            System.out.println("Укажите месяц (считаем, что январь - 0-ой месяц)");
+            month = scanner.nextInt();
+        }
 
+        System.out.println("Укажите день");
+        int day = scanner.nextInt();
+        while (day < 1 || day > 30) {
+            System.out.println("Значение дня должно быть положительным и не превышать 30");
+            System.out.println("Укажите день");
+            day = scanner.nextInt();
+        }
+
+        System.out.println("Укажите количество шагов");
+        int steps = scanner.nextInt();
+        while (steps < 0) {
+            System.out.println("Количество шагов должно быть неотрицательным");
+            System.out.println("Укажите количество шагов");
+            steps = scanner.nextInt();
+        }
+        steptracker.addSteps(month, day, steps);
+    }
+
+    private static void outputStatistics(Scanner scanner, StepTracker steptracker) {
+        System.out.println("Укажите месяц, за который вывести статистику");
+        int month = scanner.nextInt();
+        while (month < 0 || month > 11) {
+            System.out.println("Значение месяца должно быть неотрицательным и не превышать 11");
+            System.out.println("Укажите месяц");
+            month = scanner.nextInt();
+        }
+        steptracker.printStatistics(month);
+    }
+
+    private static void setGoal(Scanner scanner, StepTracker steptracker) {
+        System.out.println("Задайте дневную цель по шагам");
+        int stepsGoal = scanner.nextInt();
+        while (stepsGoal <= 0) {
+            System.out.println("Дневная цель должна быть больше или равна 0");
+            System.out.println("Задайте дневную цель по шагам");
+            stepsGoal = scanner.nextInt();
+        }
+        steptracker.modifyGoal(stepsGoal);
     }
 }
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,9 +1,6 @@
 public class StepTracker {
-
     int stepsGoal = 10000;
-
     Converter converter = new Converter();
-
     MonthData[] monthArray;
 
     public StepTracker() {
@@ -24,32 +21,45 @@ public class StepTracker {
 
     public void printStatistics(int month) {
         int[] dailySteps = monthArray[month].dailySteps;
+        int totalSteps = countTotalSteps(dailySteps);
 
         System.out.println("Статистика за " + month + " месяц");
-
         System.out.println("Количество пройденных шагов по дням:");
         for (int i = 0; i < dailySteps.length; i++) {
             System.out.print((i + 1) + " день: " + dailySteps[i] + ", ");
         }
         System.out.println("");
+        System.out.println("Общее количество шагов за месяц: " + totalSteps);
+        System.out.println("Максимальное пройденное количество шагов в месяце: " + countMaxSteps(dailySteps));
+        System.out.println("Среднее количество шагов за месяц: " + totalSteps / 30);
+        System.out.println("Пройденная дистанция в километрах: " + converter.countDistance(totalSteps));
+        System.out.println("Количество сожжённых килокалорий: " + converter.countCalories(totalSteps));
+        System.out.println("Лучшая серия из подряд идущих дней с превышением цели по шагам: " + countDaysInARow(dailySteps));
+    }
 
+    public void modifyGoal(int goal) {
+        stepsGoal = goal;
+    }
+
+    private int countTotalSteps(int[] dailySteps) {
         int totalSteps = 0;
         for (int i = 0; i < dailySteps.length; i++) {
             totalSteps = totalSteps + dailySteps[i];
         }
-        System.out.println("Общее количество шагов за месяц: " + totalSteps);
+        return totalSteps;
+    }
 
+    private int countMaxSteps(int[] dailySteps) {
         int maxSteps = 0;
         for (int i = 0; i < dailySteps.length; i++) {
             if (dailySteps[i] > maxSteps) {
                 maxSteps = dailySteps[i];
             }
         }
-        System.out.println("Максимальное пройденное количество шагов в месяце: " + maxSteps);
-        System.out.println("Среднее количество шагов за месяц: " + totalSteps / 30);
-        System.out.println("Пройденная дистанция в километрах: " + converter.countDistance(totalSteps));
-        System.out.println("Количество сожжённых килокалорий: " + converter.countCalories(totalSteps));
+        return maxSteps;
+    }
 
+    private int countDaysInARow(int[] dailySteps) {
         int daysInARow = 0;
         int maxDaysInARow = 0;
         for (int i = 0; i < dailySteps.length; i++) {
@@ -62,15 +72,6 @@ public class StepTracker {
                 daysInARow = 0;
             }
         }
-        System.out.println("Лучшая серия из подряд идущих дней с превышением цели по шагам: " + maxDaysInARow);
+        return maxDaysInARow;
     }
-
-    public void modifyGoal(int goal) {
-        stepsGoal = goal;
-    }
-
-    public int countTotalSteps () {
-
-    }
-    
 }
